f4528223cbf7fc7bf7faec6548a77231b71a1d51
Nazim Lachter <nazim@blueforest.cc>
2023-01-06 00:29:43 +0100

    license year update

--- .sandbox/search/withServer/discover.ts
+++ .sandbox/search/withServer/discover.ts
@@ -1,6 +1,6 @@
 /*
  * SocialExplorer
- * Copyright © 2019-2022 Blue Forest
+ * Copyright © 2019-2023 Blue Forest
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
--- .scripts/hooks/pre-commit.sh
+++ .scripts/hooks/pre-commit.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-COPYING_HEADER="\/*\n * SocialExplorer\n * Copyright \© 2019-2022 Blue Forest\n *\n * This program is free software: you can redistribute it and\/or modify\n * it under the terms of the GNU Affero General Public License as published\n * by the Free Software Foundation, either version 3 of the License, or\n * (at your option) any later version.\n *\n * This program is distributed in the hope that it will be useful,\n * but WITHOUT ANY WARRANTY; without even the implied warranty of\n * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n * GNU Affero General Public License for more details.\n *\n * You should have received a copy of the GNU Affero General Public License\n * along with this program. If not, see <https:\/\/www.gnu.org\/licenses\/>.\n *\/\n\n"
+COPYING_HEADER="\/*\n * SocialExplorer\n * Copyright \© 2019-2023 Blue Forest\n *\n * This program is free software: you can redistribute it and\/or modify\n * it under the terms of the GNU Affero General Public License as published\n * by the Free Software Foundation, either version 3 of the License, or\n * (at your option) any later version.\n *\n * This program is distributed in the hope that it will be useful,\n * but WITHOUT ANY WARRANTY; without even the implied warranty of\n * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n * GNU Affero General Public License for more details.\n *\n * You should have received a copy of the GNU Affero General Public License\n * along with this program. If not, see <https:\/\/www.gnu.org\/licenses\/>.\n *\/\n\n"
 
 cd $(realpath $(dirname ${BASH_SOURCE[0]}))/../..
 
-for i in $(find assembly server.ts -name "*.ts"); do
-  if ! grep -q "Copyright © 2019-2022 Blue Forest" $i; then
+for i in $(find -name "*.ts"); do
+  if ! grep -q "Copyright © 2019-2023 Blue Forest" $i; then
     echo "Adding license header to $i"
     sed -i "1s/^/$COPYING_HEADER/" $i
     git add $i
